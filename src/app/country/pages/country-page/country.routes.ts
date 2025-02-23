903ed8486c519dcedad9a065f148639d8cb32253
yacodev <cyaco33@gmail.com>
2025-02-23 10:39:13 -0500

    implement reactive forms

--- src/app/country/pages/country-page/country.routes.ts
+++
@@ -1,9 +0,0 @@
-import { CountryPageComponent } from './country-page.component';
-import { Routes } from '@angular/router';
-
-export const authRoutes: Routes = [
-  {
-    path: '',
-    component: CountryPageComponent,
-  },
-];
--- src/app/reactive/reactive.routes.ts
+++ src/app/reactive/reactive.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { BasicPageComponent } from './basic-page/basic-page.component';
-import { DynamicPageComponent } from './dynamic-page/dynamic-page.component';
-import { SwitchesPageComponent } from './switches-page/switches-page.component';
+import { BasicPageComponent } from './pages/basic-page/basic-page.component';
+import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
+import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
 
-export const authRoutes: Routes = [
+export const reactiveRoutes: Routes = [
   {
     path: '',
     children: [
