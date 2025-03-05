a6392816fba5711c0b506fdb21f82b1fff69138b
yacodev <cyaco33@gmail.com>
2025-03-05 18:14:57 -0500

    add reactive form plus components

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,10 +1,54 @@
 import { JsonPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils.';
 
 @Component({
   selector: 'app-register-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './register-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent {
+  private fb = inject(FormBuilder);
+
+  formUtils = FormUtils;
+
+  myForm: FormGroup = this.fb.group(
+    {
+      name: [
+        '',
+        [Validators.required, Validators.pattern(FormUtils.namePattern)],
+      ],
+      email: [
+        '',
+        [Validators.required, Validators.pattern(FormUtils.emailPattern)],
+        [FormUtils.checkingServerResponse],
+      ],
+      username: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.pattern(FormUtils.notOnlySpacesPattern),
+          FormUtils.notStrider,
+        ],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      password2: ['', [Validators.required, Validators.minLength(6)]],
+    },
+    {
+      validators: [this.formUtils.samePasswords('password', 'password2')],
+    }
+  );
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
+}
--- src/app/utils/form-utils..ts
+++ src/app/utils/form-utils..ts
@@ -1,6 +1,19 @@
-import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
+import {
+  AbstractControl,
+  FormArray,
+  FormGroup,
+  ValidationErrors,
+} from '@angular/forms';
+
+async function sleep() {
+  return new Promise((resolve) => setTimeout(() => resolve(true), 2000));
+}
 
 export class FormUtils {
+  static namePattern = '([a-zA-Z]+) ([a-zA-Z]+)';
+  static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  static notOnlySpacesPattern = '^[a-zA-Z0-9]+$';
+
   static isValidField(form: FormGroup, fieldName: string): boolean | null {
     return (
       !!form.controls[fieldName].errors && form.controls[fieldName].touched
@@ -42,7 +55,49 @@ export class FormUtils {
           return `Mínimo de ${errors['minlength'].requiredLength} caracteres`;
         case 'min':
           return `valor mínimo de ${errors['min'].min}`;
+        case 'email':
+          return 'el valor ingresado no es un correo electronico';
+        case 'emailTaken':
+          return 'el correo electronico ya esta en uso';
+
+        case 'noStrider':
+          return 'El nombre de usuario no puede ser strider';
+        case 'pattern':
+          if (errors['pattern'].requiredPattern === FormUtils.emailPattern) {
+            return 'el correo electronico no es valido';
+          }
+          return ' El campo no cumple con el patrón requerido de correo electronico';
+        default:
+          return `error no controlado ${key}`;
       }
+    }
+    return null;
+  }
+
+  static samePasswords(password: string, password2: string) {
+    return (formGroup: AbstractControl) => {
+      const field1value = formGroup.get(password)?.value;
+      const field2value = formGroup.get(password2)?.value;
+
+      return field1value === field2value ? null : { passwordsNotEqual: true };
+    };
+  }
+
+  static async checkingServerResponse(
+    control: AbstractControl
+  ): Promise<ValidationErrors | null> {
+    await sleep();
+    const formValue = control.value;
+    if (formValue === 'hola@mundo.com') {
+      return { emailTaken: true };
+    }
+    return null;
+  }
+
+  static notStrider(control: AbstractControl): ValidationErrors | null {
+    const formValue = control.value;
+    if (formValue === 'strider') {
+      return { noStrider: true };
     }
     return null;
   }
